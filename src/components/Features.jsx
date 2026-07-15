8c9c05c93ea224adf6b660e7a06a29c59aa5c93c
RachelDisajin <racheldisatr@gmail.com>
2026-07-15 22:00:26 +0700

    style: set main features circular logos to 135% for maximum visibility

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -8,7 +8,7 @@ export default function Features() {
                     {/* Feature 1 */}
                     <div className="feature-item">
                         <div className="feature-icon-wrapper">
-                            <img src="assets/pool.png.png" alt="Water Slides Icon" style={{ width: '75%', height: '75%', objectFit: 'contain' }} />
+                            <img src="assets/pool.png.png" alt="Water Slides Icon" style={{ width: '135%', height: '135%', objectFit: 'contain', display: 'block' }} />
                         </div>
                         <div className="feature-info">
                             <h3>
@@ -22,7 +22,7 @@ export default function Features() {
                     {/* Feature 2 */}
                     <div className="feature-item">
                         <div className="feature-icon-wrapper">
-                            <img src="assets/saung.png.png" alt="Gazebo & Rest Area Icon" style={{ width: '75%', height: '75%', objectFit: 'contain' }} />
+                            <img src="assets/saung.png.png" alt="Gazebo & Rest Area Icon" style={{ width: '135%', height: '135%', objectFit: 'contain', display: 'block' }} />
                         </div>
                         <div className="feature-info">
                             <h3>
@@ -36,7 +36,7 @@ export default function Features() {
                     {/* Feature 3 */}
                     <div className="feature-item">
                         <div className="feature-icon-wrapper">
-                            <img src="assets/kolam.png.png" alt="Swimming Pools Icon" style={{ width: '75%', height: '75%', objectFit: 'contain' }} />
+                            <img src="assets/kolam.png.png" alt="Swimming Pools Icon" style={{ width: '135%', height: '135%', objectFit: 'contain', display: 'block' }} />
                         </div>
                         <div className="feature-info">
                             <h3>
